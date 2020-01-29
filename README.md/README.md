c2aa5521e312c5539859996caa15c11273c2bc62
andrew simon <andrewsimon0007@gmail.com>
2020-01-28 21:44:37 -0600

    first commit

---
+++ README.md/README.md
@@ -0,0 +1,3 @@
+#TodoList
+
+This is a work in progress. A Todo list where you add items to the list by typing in the input box and hitting enter. To delete an item hover over it until you see the red trash can. 
---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var path = require("path");
+var app = express();
+
+var port = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on https://localhost:' + port);
+});
